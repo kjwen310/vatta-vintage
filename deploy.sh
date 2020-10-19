13c79bbf5b50d2a96ac14f708edc485dbb040440
kjwen310 <62581417+kjwen310@users.noreply.github.com>
2020-10-19 11:41:26 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 將 dist資料夾中的內容推送至遠端 hexWeek6Demo的 gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-git push -f https://github.com/kjwen310/vue-final-project.git master:gh-pages
+# 將 dist資料夾中的內容推送至遠端的 gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+git push -f https://github.com/kjwen310/vatta-vintage.git master:gh-pages
 cd -
